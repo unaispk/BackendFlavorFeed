906650784ddceb164f43a8ddd6a0ce00c0de6ae8
muhammed unais pk <127509037+unaispk@users.noreply.github.com>
2024-05-15 21:51:20 +0530

    some changes made

--- src/models/HomeRecipe.js
+++ src/models/HomeRecipe.js
@@ -2,15 +2,13 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://unaispk46:unaispk46@cluster0.irscwwt.mongodb.net/RecipeAppDatabase?retryWrites=true&w=majority&appName=Cluster0');
 const Schema = mongoose.Schema;
 
-const recipeSchema = new Schema({
+const homeRecipeSchema = new Schema({
     recipename: { type: String, required: true },
     ingredients: { type: [String] },
     instructions: { type: String },
     images: { type: [String] },
-    status: {type: String, default:"pending"},
-    userID: {type:mongoose.Types.ObjectId,ref:'user_tb'}
-
+    status: {type: String, default:"pending"}
 })
 
-const RecipeData = mongoose.model('recipe_tb', recipeSchema);
-module.exports = RecipeData; 
+const homeRecipeData = mongoose.model('homerecipe_tb', homeRecipeSchema);
+module.exports = homeRecipeData; 
--- src/routes/RecipeRoute.js
+++ src/routes/RecipeRoute.js
@@ -2,13 +2,29 @@ const express = require('express');
 const RecipeData = require('../models/RecipeModel');
 const recipeRouter = express.Router();
 recipeRouter.use(express.static('./public'));
-
 const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) { cb(null, '../frontend/public/images/uploads/') },
-    filename: function (req, file, cb) { cb(null, file.originalname) }
+const homeRecipeData = require('../models/HomeRecipe');
+const cloudinary = require('cloudinary').v2;
+
+const {CloudinaryStorage} = require('multer-storage-cloudinary');
+
+cloudinary.config({ 
+    cloud_name: "di5umyybo", 
+    api_key: "996816676243548", 
+    api_secret: "co0TneCKyDeRl4qyojhzlZZWJLo" 
+});
+
+const storageImage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {folder: "recipeprojectimages"}
 })
-const upload = multer({ storage })
+const uploadImage = multer({storage:storageImage})
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) { cb(null, '../frontend/public/images/uploads/') },
+//     filename: function (req, file, cb) { cb(null, file.originalname) }
+// })
+// const upload = multer({ storage })
 
 recipeRouter.get('/', (req, res) => {
     RecipeData.find().then((recipes) => {
@@ -16,27 +32,30 @@ recipeRouter.get('/', (req, res) => {
     }).catch((error) => {
         return res.status(400).json({ success: false, error: true, data: error })
     })
-}); 
+});
 
 // Approved recipes only 
 recipeRouter.get('/approved', (req, res) => {
-    RecipeData.find({status: 'approved'}).then((recipes) => {
+    RecipeData.find({ status: 'approved' }).then((recipes) => {
         return res.status(200).json({ success: true, error: false, data: recipes })
     }).catch((error) => {
         return res.status(400).json({ success: false, error: true, data: error })
     })
 });
 
-recipeRouter.post('/addrecipe', upload.array('images'), (req, res) => {
-    console.log("files",req.files);
+recipeRouter.post('/addrecipe/:id', uploadImage.array('images'), (req, res) => {
+    console.log(req.params.id);
+    console.log("files", req.files);
     console.log(req.body.ingredients)
-    const recipeDetails = { recipename: req.body.recipename, 
-        ingredients: req.body.ingredients? (req.body.ingredients.split(',')).map(ingredients => ingredients): '',
-        
+    const recipeDetails = {
+        recipename: req.body.recipename,
+        ingredients: req.body.ingredients ? (req.body.ingredients.split(',')).map(ingredients => ingredients) : '',
         // ingredients: req.body.ingredients ? (req.body.ingredients.includes(',') ? req.body.ingredients.split(',').map(ingredient => ingredient.trim()) : [req.body.ingredients.trim()]) : [],
         instructions: req.body.instructions,
         // images: req.files.map(file => file.filename) }
-        images: req.files ? req.files.map((file) => file.filename) : '' }
+        images: req.files ? req.files.map((file) => file.path) : '',
+        userID: req.params.id
+    }
     console.log('recipeDetails ---->>', recipeDetails);
 
     RecipeData(recipeDetails).save().then((recipe) => {
@@ -46,9 +65,33 @@ recipeRouter.post('/addrecipe', upload.array('images'), (req, res) => {
     })
 });
 
+// Add Recipe to home screen
+
+recipeRouter.post('/addrecipetohome', uploadImage.array('images'), (req, res) => {
+    console.log("files", req.files);
+    console.log(req.body.ingredients)
+    const recipeDetails = {
+        recipename: req.body.recipename,
+        ingredients: req.body.ingredients ? (req.body.ingredients.split(',')).map(ingredients => ingredients) : '',
+
+        // ingredients: req.body.ingredients ? (req.body.ingredients.includes(',') ? req.body.ingredients.split(',').map(ingredient => ingredient.trim()) : [req.body.ingredients.trim()]) : [],
+        instructions: req.body.instructions,
+        // images: req.files.map(file => file.filename) }
+        images: req.files ? req.files.map((file) => file.filename) : ''
+    }
+    console.log('recipeDetails ---->>', recipeDetails);
+
+    homeRecipeData(recipeDetails).save().then((recipe) => {
+        return res.status(200).json({ success: true, error: false, message: "Recipe added" })
+    }).catch((error) => {
+        return res.status(400).json({ success: false, error: true, data: error })
+    })
+});
+
+
 recipeRouter.get('/viewsinglerecipe/:recipeId', (req, res) => {
     const id = req.params.recipeId;
-    RecipeData.findOne({_id : id}).then((recipeDetails) => {
+    RecipeData.findOne({ _id: id }).then((recipeDetails) => {
         return res.status(200).json({ success: true, error: false, data: recipeDetails })
     }).catch((error) => {
         return res.status(400).json({ success: false, error: true, data: error })
@@ -57,7 +100,7 @@ recipeRouter.get('/viewsinglerecipe/:recipeId', (req, res) => {
 
 recipeRouter.post('/approve/:recipeId', (req, res) => {
     const id = req.params.recipeId;
-    RecipeData.updateOne({ _id: id }, { $set: {status: 'approved'} }).then((recipe) => {
+    RecipeData.updateOne({ _id: id }, { $set: { status: 'approved' } }).then((recipe) => {
         return res.status(200).json({ success: true, error: false, data: recipe, message: 'approved' })
     }).catch((error) => {
         return res.status(400).json({ success: false, error: true, data: error })
@@ -66,11 +109,21 @@ recipeRouter.post('/approve/:recipeId', (req, res) => {
 
 recipeRouter.post('/reject/:recipeId', (req, res) => {
     const id = req.params.recipeId;
-    RecipeData.updateOne({ _id: id }, { $set: {status: 'rejected'} }).then((recipe) => {
-        return res.status(200).json({ success: true, error: false, data: recipe , message: 'rejected' })
+    RecipeData.updateOne({ _id: id }, { $set: { status: 'rejected' } }).then((recipe) => {
+        return res.status(200).json({ success: true, error: false, data: recipe, message: 'rejected' })
     }).catch((error) => {
         return res.status(400).json({ success: false, error: true, data: error })
     })
 });
+
+recipeRouter.get('/view-user-recipes/:userid', (req, res) => {
+    const userID = req.params.userid;
+    console.log(userID);
+    RecipeData.find({userID: userID }).then((usersrecipes) => {
+        return res.status(200).json({ success: true, error: false, data: usersrecipes })
+    }).catch((error) => {
+        return res.status(400).json({ success: false, error: true, data: error })
+    })
+})
 
 module.exports = recipeRouter;
